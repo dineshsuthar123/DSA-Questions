5407625139e8e653dcef5fb7b0115b61bca01c73
dineshsuthar123 <dinesh.suthar18sld@gmail.com>
2024-11-23 20:04:04 +0530

    Solution for rotating the box in leetcode

---
+++ src/LeetCode_DailyQuestions/Rotating_the_Box.java
@@ -0,0 +1,50 @@
+package LeetCode_DailyQuestions;
+
+import java.util.Arrays;
+
+public class Rotating_the_Box {
+    public static void main(String[] args) {
+        Solution1 solution = new Solution1();
+
+        // Example input
+        char[][] box = {
+                {'#', '#', '*', '.', '.'},
+                {'#', '#', '#', '*', '.'},
+                {'#', '#', '.', '#', '*'}
+        };
+
+        char[][] result = solution.rotateTheBox(box);
+
+        System.out.println("Rotated Box:");
+        for (char[] row : result) {
+            System.out.println(Arrays.toString(row));
+        }
+    }
+}
+
+class Solution1 {
+    public char[][] rotateTheBox(char[][] box) {
+        int ROWS = box.length;
+        int COLS = box[0].length;
+
+        char[][] res = new char[COLS][ROWS];
+
+        for (char[] Fill : res) {
+            Arrays.fill(Fill, '.');
+        }
+
+        for (int r = 0; r < ROWS; r++) {
+            int i = COLS - 1;
+            for (int c = COLS - 1; c >= 0; c--) {
+                if (box[r][c] == '#') {
+                    res[i][ROWS - r - 1] = '#';
+                    i--;
+                } else if (box[r][c] == '*') {
+                    res[c][ROWS - r - 1] = '*';
+                    i = c - 1;
+                }
+            }
+        }
+        return res;
+    }
+}
